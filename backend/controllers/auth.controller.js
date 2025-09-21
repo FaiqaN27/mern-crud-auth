07cc29a69ecd47914872841670fb1ab96b3c44e4
Faiqa Nadeem <faiqanadeem6725@gmail.com>
2025-09-22 04:09:58 +0500

    create sign in api route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const handleSignup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -20,5 +22,31 @@ export const handleSignup = async (req, res, next) => {
   catch (error) {
     next(error);
   }
+}
 
+export const handleSignin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, 'User not found'));
+
+    //comparing password 
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Invalid Credentials'));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+
+    //expiry date - 1 hour
+    const expiryDate = new Date(Date.now() + 3600000);
+
+    //pass here expiry 
+    res
+      .cookie('token', token, { httpOnly: true }, expiryDate)
+      .status(200)
+      .json(rest);
+  }
+  catch (error) {
+    next(error);
+  }
 }
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { handleSignup } from "../controllers/auth.controller.js";
+import { handleSignup, handleSignin } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", handleSignup)
+router.post("/signin", handleSignin);
 
 export default router;
